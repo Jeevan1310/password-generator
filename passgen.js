752274224001214881eac7f11f683aba0874ae75
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-08-24 11:47:21 +0530

    Added functions

---
+++ passgen.js
@@ -0,0 +1,43 @@
+const PWEL = document.getElementById("pw");
+
+const copyEl = document.getElementById("copy");
+
+const lenEl = document.getElementById("len");
+
+const upperEl = document.getElementById("upper");
+
+const lowerEl = document.getElementById("lower"); const symbolEl = document.getElementById("symbol"); const generateEl = document.getElementById("generate");
+
+const numberEl document.getElementById("number"); const upperLetters = "ABCDEFGHIJKLMNOPQSRTUVWXYZ";
+
+const lowerLetters "abcdefghijklmnopqrstuvwxyz"; const numbers = "0123456789";
+
+const symbol = "~!@#$%^&*( ) _+=|";
+
+function getLowercase() { return lower Letters[Math.floor(Math.random() *
+
+lowerLetters.length)]; } function getUppercase() {
+
+return upperLetters [Math.floor(Math.random()
+
+upperLetters.length)];
+
+} function getNumber() {
+
+return numbers[Math.floor(Math.random() numbers.length)];
+
+} function getSymbol() {
+
+return symbol[Math.floor(Math.random() * symbol.length)]; }
+
+function generatePassword() {
+
+const len = lenEl.value; let password = "";
+
+for (let i = 0; i < len; i++) { const x = generateX();
+
+password += x;
+
+} PWEL.innerText = password;
+
+}
